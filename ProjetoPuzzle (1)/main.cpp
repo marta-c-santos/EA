8e7b578a2d1afe4a51b7733f56578b1f4ff74f8b
Marta Santos <60897114+marta-c-santos@users.noreply.github.com>
2022-03-06 21:18:03 +0000

    c++

---
+++ ProjetoPuzzle (1)/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+// void rotacao(peca);
+
+int main() {
+
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+    int ntest, npecas, nlin, ncol;
+    std::cin >> ntest;
+    std::cin >> npecas >> nlin >> ncol;
+
+    std::cout << ntest << "....\n" << npecas << "---\n"<< nlin << "::\n" << ncol << "///\n";
+    for (int i = 0; i < ntest; i++) {
+        //int array[npecas];
+
+
+        std::cout << "uwehfefdjnsdjk\n";
+        for (int j = 0; j < npecas; j++) {
+            std::cout << "lllllllllllllllllllll\n";
+            //std::cin >> array[j];
+            std::vector<std::vector<int>> matrix;
+            std::cout << array[j] << "\n";
+        }
+
+        //std::cin >> npecas >> nlin >> ncol;
+        std::cout << array << "\n";
+    }
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+//void rotacao(peca) {
+
+//}
