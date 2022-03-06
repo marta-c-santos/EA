f05045b40a0cc159256c0b0e7ea1ee540dfd1a20
Marta Santos <60897114+marta-c-santos@users.noreply.github.com>
2022-03-06 21:16:59 +0000

    comentado

--- ProjetoPuzzle (1)/main.py
+++ ProjetoPuzzle (1)/main.py
@@ -9,22 +9,51 @@ def outln(n):
     stdout.write(str(n))
     stdout.write("\n")
 
+# ter a solucao como variavel global para não ser preciso passar os parametros das linhas e colunas
 
 def puzzle(pecas, nlin, ncol):
     solucao = [[[0 for i in range(4)] for j in range(ncol)] for k in range(nlin)]
+
+    auxlin = 0
+    auxcol = 0
+    pecaPosta = 0 # nao foi colocada, 1 se for
 
     print("solucao", solucao)
     solucao[0][0] = pecas[0]
     #print("solucao", solucao)
     # print("pecas:", len(pecas))
-    for i in range(1, len(pecas)):
-        nlin = 0
-        ncol = 0
-        # direita
-        if (pecas[i][1] == solucao[ncol][nlin][0] and pecas[i][2] == solucao[ncol][nlin][3]):
-            nlin += 1
-            ncol += 1
-            solucao[ncol][nlin] = pecas[i]
+
+    while(pecaPosta == 0):
+        for i in range(1, len(pecas)):
+
+            # direita
+            if (pecas[i][1] == solucao[auxcol][auxlin][0] and pecas[i][2] == solucao[auxcol][auxlin][3]):
+                auxlin += 1
+                auxcol += 1
+                solucao[auxcol][auxlin] = pecas[i]
+
+
+            #fazer para acrescentar na linha
+            # nlin++
+
+            #tentar colocar a peça na mesma
+            if (auxlin <= nlin):
+                if (pecas[i][1] == solucao[auxcol][auxlin][0] and pecas[i][2] == solucao[auxcol][auxlin][3]):
+                    auxlin += 1
+                    auxcol += 1
+                    solucao[auxcol][auxlin] = pecas[i]
+                    pecaPosta = 1
+                else:
+                    pecaR = rotacao(pecas[i])
+                    pecaPosta = 0
+
+
+            #fazer para acrescentar na coluna
+            # ncol++
+            # tentar com todas as pecas
+            # senao rodar 1 a 1 e experimentar todas até alguma dar ou até ao fim
+
+
 
     print(solucao)
     # 0 1    0 1
@@ -39,6 +68,8 @@ def rotacao(peca):
     for i in range(len(peca) - 1, -1, -1):
         peca[i] = peca[i - 1]
     peca[0] = aux
+
+    return peca
 
 
 if __name__ == "__main__":
