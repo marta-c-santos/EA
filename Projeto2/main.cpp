db989eb389aafefc4d2f0a10638be0a6e189c5b7
FabioVaqueiro <66789343+FabioVaqueiro@users.noreply.github.com>
2022-04-13 17:36:15 +0100

    Add files via upload

--- Projeto2/main.cpp
+++ Projeto2/main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 using namespace std;
-
+int minimo;
 int funcao(vector<vector<int>> nos, int atual);
 
 int main() {
@@ -22,7 +22,8 @@ int main() {
             linha.clear();
 
         } else {
-            funcao(nos, 0);
+            int res = funcao(nos, 0);
+            cout << "Resultado: " << res << "\n";
             nos.clear();
         }
     }
@@ -34,19 +35,25 @@ int main() {
 int funcao(vector<vector<int>> nos, int atual) {
     //vector<int> final_in;
     //vector<int> final_out;
+    int final_in = 0, final_out = 0;
     vector<int> recrutados = nos.at(atual);
 
     for (int rec: recrutados) {
         cout << "rec: " << rec << "\n";
     }
 
-    int final_in = 0, final_out = 0;
     if (nos.size() <= 1) { //apenas 1 no
+        cout << "-----------------------------------primeiro if\n";
         return 0;
     }
 
     if(recrutados.size() < 2){ //sem no filho
+        cout << "----------------------------------segundo if\n";
         return 0;
+    }
+
+    if(minimo != -1){
+        return minimo;
     }
 
     //in
@@ -54,32 +61,33 @@ int funcao(vector<vector<int>> nos, int atual) {
     cout << "tamanho rec: " << recrutados.size() << "\n";
 
     for (int i = 1; i < (int) recrutados.size() - 1; i++) {
-        cout << "--------------i: " << i << "\n";
-        int a = funcao(nos, recrutados[i]);
+        cout << "----------rec: " << recrutados[1] << "\n";
+        int a = 1 + funcao(nos, recrutados[i]);
         cout << "a: " << a << "\n";
         final_in += a;
         cout << "final_in: " << final_in << "\n";
     }
 
-
     //out
-
     for (int i = 1; i < (int) recrutados.size() - 1; i++) {
         cout << "---->recrutdossss: " << recrutados[i] << "\n";
 
         cout << "tamanho da linha: " << nos[i].size() << "\n";
         if (nos[i].size() >= 3) { //verificar se tem no filho
             cout << "if\n";
-            for (int j = 1; j < (int) nos.at(recrutados[i]).size() -1 ; j++) {
+            for (int j = 1; j < nos.at(recrutados[i]).size() - 1; j++) {
                 cout << "!!!!!!!!!!!!!recrutadosss: " << nos.at(recrutados[j])[i] << "\n";
-                final_out += funcao(nos, nos.at(recrutados[j])[i]);
+                final_out += 1 + funcao(nos, nos.at(recrutados[j])[i]);
                 cout << "final_out: " << final_in << "\n";
             }
         }
     }
 
     cout << "custo2: " << recrutados.back() << "\n";
-    return min(final_in, final_out);
+    minimo = min(final_in, final_out);
+
+    cout << "minimo: " << minimo << "\n";
+    return minimo;
 }
 
 
