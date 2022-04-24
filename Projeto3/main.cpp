83606f16a909acaeed0608c1535bd1f0f39a95b8
FabioVaqueiro <66789343+FabioVaqueiro@users.noreply.github.com>
2022-04-24 17:23:42 +0100

    Valid | Invalid

--- Projeto3/main.cpp
+++ Projeto3/main.cpp
@@ -1,7 +1,12 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 using namespace std;
+
+void funcao(vector<vector<int>> operacoes, int stat);
+
+bool valid(vector<vector<int>> operacoes);
 
 int main() {
     int n_op, time, n_dep, dep, stat;
@@ -32,5 +37,65 @@ int main() {
     cout << stat;
      */
 
-    
+    funcao(operacoes, stat);
+
+}
+
+void funcao(vector<vector<int>> operacoes, int stat) {
+    /*
+     * .
+     * .
+     * .
+     */
+
+    // verificacao das estatisticas
+    if (valid(operacoes)) {
+        if (stat == 0) {
+            cout << "VALID";
+        } else if (stat == 1) {
+
+        } else if (stat == 2) {
+
+        } else if (stat == 3) {
+
+        } else { // tem de dar erro
+
+        }
+    } else {
+        cout << "INVALID";
+    }
+}
+
+bool valid(vector<vector<int>> operacoes) {
+    bool no_dep = false;
+    vector<int> not_terminal;
+
+    for (int i = 0; i < operacoes.size(); i++) {
+        if (operacoes[i].size() == 2) { // nao tem dependencias
+            if (!no_dep) {
+                no_dep = true;
+            } else {
+                return false;
+            }
+        }
+
+        int no = i + 1;
+        for (int j = 0; j < operacoes.size(); j++) {
+            for (int k = 2; k < operacoes[j].size(); k++) {
+                auto r = find(not_terminal.begin(), not_terminal.end(), no); //verifica se no ja nao e terminal
+                if (no == operacoes[j][k] and r == not_terminal.end()) {
+                    not_terminal.push_back(no);
+                }
+            }
+        }
+    }
+
+    // compara retira os nos que nao sao terminais
+    // descubrindo o nº de nos terminais
+    if (operacoes.size() - not_terminal.size() > 1) {
+        //cout << "nos terminais: " << operacoes.size() - not_terminal.size() << "\n";
+        return false;
+    }
+
+    return true;
 }
